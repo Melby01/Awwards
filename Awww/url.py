c462520b699ef2fd1689e6bdb5698e11c04c4be1
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-17 02:26:42 +0300

    Update the admin part

--- Awww/url.py
+++ Awww/url.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^registration', views.register, name='register')
+    url(r'^registration', views.register, name='register'),
+    url(r'^$',views.index,name='index'),
+
 ]
