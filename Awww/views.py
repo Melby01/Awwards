9b1c24bfb00ca2ae49189dc0fd9e11e3ffbd4185
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-17 00:21:50 +0300

    Update the registration part

--- Awww/views.py
+++ Awww/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import Registration
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+     form = Registration(request.POST)
+     if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('login')
+    else:
+      form = Registration()
+    return render(request,'registration/registration_form.html',{"form":form})
