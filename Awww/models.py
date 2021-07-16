5d09459889041452a66bf429e9262582cd139b31
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-17 01:58:00 +0300

    Make the 2nd model

--- Awww/models.py
+++ Awww/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
 # Create your models here.
 
 class Projects(models.Model):
@@ -41,3 +45,27 @@ class Projects(models.Model):
             
     def __str__(self):
         return self.description
+    
+class Profile(models.Model):
+    profile_pic= models.ImageField(upload_to='profile/' )
+    bio = models.TextField(blank=True)
+    user_id = models.OneToOneField(User, null= True, on_delete=models.CASCADE)
+    more_info = models.TextField(blank=False, null=True)
+    
+    def __str__(self):
+        return self.bio
+    
+    def save_user(self):
+        self.save()
+        
+        
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+     
+    
