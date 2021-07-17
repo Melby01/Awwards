eb6a46d4e17afd0102c1ec1d159b9dba3ffac84a
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-17 03:24:19 +0300

    Update the regitration to be the home page

--- Awww/url.py
+++ Awww/url.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^registration', views.register, name='register'),
-    url(r'^$',views.index,name='index'),
+    url(r'^', views.register, name='register'),
+    url(r'^index$',views.index,name='index'),
 
 ]
